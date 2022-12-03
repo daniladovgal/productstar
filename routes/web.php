1cfb25cd2aece86770b22523197a76416a9cbd1e
Danila Dpvgal <danildovgal@gmail.com>
2022-12-03 13:07:36 +0300

    Polugotovo

--- routes/web.php
+++ routes/web.php
@@ -13,6 +13,16 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/weather', function () {
+    return view('weather');
 });
+
+Route::get('/users/{id?}', function () {
+    return view('users');
+});
+
+Route::get('/fill', 'FillController');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
